e3e62c109663a408d0dac6712e60b813e5e49285
PACY2 <cmouhrach@gmail.com>
2023-07-05 14:20:14 +0000

    reset

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="h-16 px-4 lg:p-0 flex items-center justify-center bg-white dark:bg-dark-blue">
-      <span>© Choaib Mouhrach 2022 - 2023.</span>
-    </footer>
-  );
-};
-
-export default Footer;
--- src/components/Title.tsx
+++
@@ -1,9 +0,0 @@
-type TitleProps = {
-  children: React.ReactNode;
-};
-
-const Title = ({ children }: TitleProps) => {
-  return <h2 className="text-xl font-semibold py-4 ">{children}</h2>;
-};
-
-export default Title;
