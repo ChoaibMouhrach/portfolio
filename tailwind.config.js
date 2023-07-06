a4f0de8f9728ba3d6519e9294dc7e10b77b22388
PACY2 <cmouhrach@gmail.com>
2023-07-06 12:12:20 +0000

    changing container width

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,12 @@ module.exports = {
     extend: {
       container: {
         center: true,
-        padding: {
-          DEFAULT: '1rem',
-          sm: '2rem',
-          lg: '4rem',
-          xl: '5rem',
-          '2xl': '6rem',
+        // default breakpoints but with 40px removed
+        screens: {
+          sm: '600px',
+          md: '800px',
+          lg: '1000px',
+          xl: '1200px',
         },
       },
       backgroundImage: {
