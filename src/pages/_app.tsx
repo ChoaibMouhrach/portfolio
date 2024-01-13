6f2ba9dbc232d79371a94890a510bbda82494270
ChoaibMouhrach <cmouhrach@gmail.com>
2024-01-13 17:15:18 +0000

    feat:add ads

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-4284624709758827" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
